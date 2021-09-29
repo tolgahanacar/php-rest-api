bac66a6805c0ecb240d88182cd982941fa8b585e
tolgahanacar <47753083+tolgahanacar@users.noreply.github.com>
2021-09-29 13:26:24 +0300

    Major Upgrade :alien:
    Major Upgrade :alien:

--- index.php
+++ index.php
@@ -12,11 +12,11 @@ if ($request_method === "GET") { //GET Method
         if (empty($id)) {
             $jsonArray['error'] = true;
             $jsonArray['errorMessage'] = "Invalid or null value!";
-            $_code = 403;
+            $_code = 406;
         } else if (!is_numeric($id)) {
             $jsonArray['error'] = true;
             $jsonArray['errorMessage'] = "The request must be 'numeric'";
-            $_code = 403;
+            $_code = 406;
         } else {
             $control = $db->prepare("SELECT * FROM users WHERE id = :id");
             $control->bindParam(":id", $id, PDO::PARAM_INT);
@@ -31,7 +31,7 @@ if ($request_method === "GET") { //GET Method
             } else {
                 $jsonArray['error'] = true;
                 $jsonArray['errorMessage'] = "No value found for your request!";
-                $_code = 403;
+                $_code = 404;
             }
 
 
@@ -45,37 +45,38 @@ if ($request_method === "GET") { //GET Method
         $_code = 200;
     }
 } else if ($request_method === "POST") { //POST Method
+    $userName  = Security($_POST['username']);
     $firstName = Security($_POST['first_name']);
     $lastName  = Security($_POST['last_name']);
-    $age       = Security($_POST['age']);
-    $city      = Security($_POST['city']);
-    $ip        = Security($_POST['ip']);
-    if (empty($firstName) || empty($lastName) || empty($age) || empty($city) || empty($ip)) {
+    $email     = Security($_POST['email']);
+    if (empty($userName) || empty($firstName) || empty($lastName) || empty($email)) {
         $jsonArray['error'] = true;
         $jsonArray['errorMessage'] = "Invalid or null value!";
-        $_code = 403;
+        $_code = 406;
     } else if (is_numeric($firstName) || is_numeric($lastName)) {
         $jsonArray['error'] = true;
         $jsonArray['errorMessage'] = "first or last name cannot contain numeric values.";
-        $_code = 403;
+        $_code = 406;
+    } else if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+        $jsonArray['error'] = true;
+        $jsonArray['errorMessage'] = "invalid email address";
+        $_code = 406;
     } else {
-        $add = $db->prepare("INSERT INTO users (first_name, last_name, age, city, ip) VALUES (:fname, :lname, :age, :city, :ip)");
+        $add = $db->prepare("INSERT INTO users (username, first_name, last_name, email) VALUES (:uname, :fname, :lname, :email)");
+        $add->bindParam(":uname", $userName, PDO::PARAM_STR);
         $add->bindParam(":fname", $firstName, PDO::PARAM_STR);
         $add->bindParam(":lname", $lastName, PDO::PARAM_STR);
-        $add->bindParam(":age", $age, PDO::PARAM_INT);
-        $add->bindParam(":city", $city, PDO::PARAM_STR);
-        $add->bindParam(":ip", $ip, PDO::PARAM_STR);
+        $add->bindParam(":email", $email, PDO::PARAM_STR);
         $add->execute();
         if ($db->lastInsertId()) {
             $jsonArray["send_data"] = "Data sending is successful";
+            $jsonArray['username'] = $userName;
             $jsonArray['first_name'] = $firstName;
             $jsonArray['last_name'] = $lastName;
-            $jsonArray['age'] = $age;
-            $jsonArray['city'] = $city;
-            $jsonArray['ip'] = $ip;
+            $jsonArray['email'] = $email;
         } else {
             $jsonArray['error'] = true;
-            $_code = 404;
+            $_code = 403;
             $jsonArray['errorMessage'] = "Data sending failed!";
         }
     }
@@ -84,11 +85,11 @@ if ($request_method === "GET") { //GET Method
     if (empty($id)) {
         $jsonArray['error'] = true;
         $jsonArray['errorMessage'] = "Invalid or null value!";
-        $_code = 403;
+        $_code = 406;
     } else if (!is_numeric($id)) {
         $jsonArray['error'] = true;
         $jsonArray['errorMessage'] = "The request must contain a numeric value!";
-        $_code = 403;
+        $_code = 406;
     } else {
         $control = $db->prepare("SELECT * FROM users WHERE id = :id");
         $control->bindParam(":id", $id, PDO::PARAM_INT);
@@ -104,43 +105,45 @@ if ($request_method === "GET") { //GET Method
             } else {
                 $jsonArray['error'] = true;
                 $jsonArray['deleteid'] = $id;
-                $_code = 404;
+                $_code = 403;
                 $jsonArray['errorMessage'] = "Deletion failed.";
             }
         } else {
             $jsonArray['error'] = true;
             $jsonArray['errorMessage'] = "No value found for your request!";
-            $_code = 403;
+            $_code = 404;
         }
     }
 } else if ($request_method === "PUT") {
     $put_req = json_decode(file_get_contents("php://input"));
     $id = $put_req->id;
+    $userName = $put_req->username;
     $firstName = $put_req->first_name;
     $lastName  = $put_req->last_name;
-    $age = $put_req->age;
-    $city = $put_req->city;
-    $ip   = $put_req->ip;
-    if (empty($id) || empty($firstName) || empty($lastName) || empty($age) || empty($city) || empty($ip)) {
+    $email = $put_req->email;
+    if (empty($id) || empty($userName) || empty($firstName) || empty($lastName) || empty($email)) {
         $jsonArray['error'] = true;
         $jsonArray['errorMessage'] = "Invalid or null value!";
-        $_code = 403;
+        $_code = 406;
     } else if (is_numeric($firstName) || is_numeric($lastName)) {
         $jsonArray['error'] = true;
         $jsonArray['errorMessage'] = "first or last name cannot contain numeric values.";
-        $_code = 403;
+        $_code = 406;
+    } else if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+        $jsonArray['error'] = true;
+        $jsonArray['errorMessage'] = "invalid email address";
+        $_code = 406;
     } else {
         $control = $db->prepare("SELECT * FROM users WHERE id = :id");
         $control->bindParam(":id", $id, PDO::PARAM_INT);
         $control->execute();
         $controlCount = $control->rowCount();
         if ($controlCount > 0) {
-            $update = $db->prepare("UPDATE users SET first_name = :fname, last_name = :lname, age = :age, city = :city, ip = :ip WHERE id = :id");
+            $update = $db->prepare("UPDATE users SET username = :uname, first_name = :fname, last_name = :lname, email = :email WHERE id = :id");
+            $update->bindParam(":uname", $userName, PDO::PARAM_STR);
             $update->bindParam(":fname", $firstName, PDO::PARAM_STR);
             $update->bindParam(":lname", $lastName, PDO::PARAM_STR);
-            $update->bindParam(":age", $age, PDO::PARAM_INT);
-            $update->bindParam(":city", $city, PDO::PARAM_STR);
-            $update->bindParam(":ip", $ip, PDO::PARAM_STR);
+            $update->bindParam(":email", $email, PDO::PARAM_STR);
             $update->bindParam("id", $id, PDO::PARAM_INT);
             $update->execute();
             if ($update) {
@@ -148,15 +151,15 @@ if ($request_method === "GET") { //GET Method
                 $jsonArray['affectedId'] = $id;
             } else {
                 $jsonArray['error'] = true;
-                $_code = 404;
+                $_code = 403;
                 $jsonArray['errorMessage'] = "Data sending failed!";
             }
         }
     }
 } else {
     $jsonArray['error'] = true;
-    $_code = 404;
-    $jsonArray['errorMessage'] = "Invalid request or undefined method";
+    $_code = 405;
+    $jsonArray['errorMessage'] = "Method Not Allowed";
 }
 
 
